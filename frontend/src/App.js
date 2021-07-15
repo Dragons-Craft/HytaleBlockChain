f49094620dff00c0e1bedb3680dd15c0c496fba4
Anjelica <ambrosio.anjelica@gmail.com>
2021-07-15 13:45:18 -0700

    plugged in meta data to the listings

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,6 @@ import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
 import About from "./components/About/About";
 import Listings from "./components/Listings/Listings";
-import Contracts from "./components/Contracts/Contracts";
 
 function App() {
   return (
--- frontend/src/components/Listings/Listings.js
+++ frontend/src/components/Listings/Listings.js
@@ -15,8 +15,8 @@ function Listings() {
             width="50%"
             height="60%"
           ></img>
-          <h3>"Alterverses"</h3> <h3>Price: 0 ETH</h3>
-          <h4>Description: </h4>
+          <h3>"Alterverses"</h3> <h3>Starting Bid: 0 ETH</h3>
+          <h4>Description: Hytale Concept Art - Alterverses </h4>
           <h4>Current Owner: </h4>
           <a href="/">Take Me Home!</a>
         </div>
@@ -24,7 +24,7 @@ function Listings() {
         <div className="sample1">
           <img src="images/gaia.jpeg" alt="gaia" width="50%" height="60%"></img>
           <h3>"Gaia"</h3> <h3>Price: 0 ETH</h3>
-          <h4>Description: </h4>
+          <h4>Description: Hytale Mobile Wallpaper - Gaia</h4>
           <h4>Current Owner: </h4>
           <a href="/">Take Me Home!</a>
         </div>
