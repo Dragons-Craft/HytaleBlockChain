958951fba01a8a8d33d1688e519d7342a4d926bd
Christopher Barnes <chris.barnes.2000@me.com>
2021-07-15 03:12:30 -0700

    [Update]: NFT Listings

--- src/components/Listings/Listings.js
+++ src/components/Listings/Listings.js
@@ -5,8 +5,41 @@ class Listings extends Component {
 
   render() {
     return (
-      <div id="content">
-        <h1>Add Product</h1>
+      <section id="nfts">
+        <div className="title">NFTs</div>
+
+        <div className="grid">
+          {this.props.products.map((product, key) => {
+            return (
+              <div className="sample1" key={key}>
+                {/* <img src="media/imgs/{product.name}.jpeg" alt={product.name} width="50%" height="60%"></img> */}
+                <img src="media/imgs/Gaia.jpeg" alt={product.name} width="50%" height="60%"></img>
+                
+                <h3>{product.name}</h3> <h3>Price: {window.web3.utils.fromWei(product.price.toString(), 'Ether')} Eth</h3>
+                <h4>Description: {product.description}</h4>
+                <h4>Current Owner: {product.owner}</h4>
+                
+                <a href="/">Take Me Home!</a>
+                <br />
+
+                {!product.purchased
+                  ? <button
+                    name={product.id}
+                    value={product.price}
+                    onClick={(event) => {
+                      this.props.purchaseProduct(event.target.name, event.target.value)
+                    }}
+                  >
+                    Buy
+                  </button>
+                  : null
+                }
+              </div>
+            )
+          })}
+        </div>
+
+        <div className="title">Add NFTs Product</div>
         <form onSubmit={(event) => {
           event.preventDefault()
           const name = this.productName.value
@@ -33,71 +66,8 @@ class Listings extends Component {
           </div>
           <button type="submit" className="btn btn-primary">Add Product</button>
         </form>
-        <p>&nbsp;</p>
 
-        <section id="nfts"></section>
-        <div className="title">NFTs</div>
-        <div className="grid">
-          <div className="sample1">
-            <img
-              src="images/alterverses.jpeg"
-              alt="alterverses"
-              width="50%"
-              height="60%"
-            ></img>
-            <h3>"Alterverses"</h3> <h3>Price: 0 ETH</h3>
-            <h4>Description: </h4>
-            <h4>Current Owner: </h4>
-            <a href="/">Take Me Home!</a>
-          </div>
-
-          <div className="sample1">
-            <img src="images/gaia.jpeg" alt="gaia" width="50%" height="60%"></img>
-            <h3>"Gaia"</h3> <h3>Price: 0 ETH</h3>
-            <h4>Description: </h4>
-            <h4>Current Owner: </h4>
-            <a href="/">Take Me Home!</a>
-          </div>
-        </div>
-
-        <table className="table">
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Name</th>
-              <th scope="col">Price</th>
-              <th scope="col">Owner</th>
-              <th scope="col"></th>
-            </tr>
-          </thead>
-          <tbody id="productList">
-            {this.props.products.map((product, key) => {
-              return (
-                <tr key={key}>
-                  <th scope="row">{product.id.toString()}</th>
-                  <td>{product.name}</td>
-                  <td>{window.web3.utils.fromWei(product.price.toString(), 'Ether')} Eth</td>
-                  <td>{product.owner}</td>
-                  <td>
-                    {!product.purchased
-                      ? <button
-                        name={product.id}
-                        value={product.price}
-                        onClick={(event) => {
-                          this.props.purchaseProduct(event.target.name, event.target.value)
-                        }}
-                      >
-                        Buy
-                      </button>
-                      : null
-                    }
-                  </td>
-                </tr>
-              )
-            })}
-          </tbody>
-        </table>
-      </div>
+      </section>
     );
   }
 }
