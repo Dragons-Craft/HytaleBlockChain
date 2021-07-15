6595f237b1ae4c792d7513f076cc1f926c94f9a3
Anjelica <ambrosio.anjelica@gmail.com>
2021-07-14 22:09:31 -0700

    updated front end with new look

--- frontend/src/components/About/About.js
+++ frontend/src/components/About/About.js
@@ -4,7 +4,7 @@ import "./About.css";
 function About() {
 return (
   <div>
-    // About Section
+    // About
     <section id="about"></section>
     <div className="title">WHAT IS HYTALE?</div>
     <div className="about-container">
